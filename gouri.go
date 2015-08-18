bdb765902adc562d4c15b8e72f8aed8a47359b19
jarryliu <jarrysix@gmail.com>
2015-08-18 09:58:03 +0800

    commit'

--- gouri.go
+++ gouri.go
@@ -7,88 +7,89 @@
  * history :
  */
 package main
+
 import (
-	"net/http"
+	"bufio"
+	"encoding/json"
 	"flag"
 	"fmt"
-	"log"
-	"os"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 	"path/filepath"
 	"strings"
-	"encoding/json"
-	"bufio"
 )
 
 var (
 	ConfBasePath = "conf/"
 )
 
-func main(){
+func main() {
 	var port int
 	var dir string
-	flag.IntVar(&port,"port",8302,"port")
-	flag.StringVar(&dir,"dir","conf/","config file")
+	flag.IntVar(&port, "port", 8302, "port")
+	flag.StringVar(&dir, "dir", "conf/", "config file")
 	ConfBasePath = dir
 
 	app := http.Server{
-		Addr:fmt.Sprintf(":%d",port),
-		Handler:getHandler(),
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: getHandler(),
 	}
 
-	log.Printf("[ Service] - running on port %s\n",app.Addr)
-	if err := app.ListenAndServe();err != nil{
-		log.Fatalf(" Server aborted!reason:%s\n",err.Error())
+	log.Printf("[ Service] - running on port %s\n", app.Addr)
+	if err := app.ListenAndServe(); err != nil {
+		log.Fatalf(" Server aborted!reason:%s\n", err.Error())
 	}
 }
 
 type Item struct {
-	Host        string                `json:"host"`
-	AllLocation string                    `json:"to"`
-	Map         map[string]string    `json:"map"`
+	Host        string            `json:"host"`
+	AllLocation string            `json:"to"`
+	Map         map[string]string `json:"map"`
 }
 
-type ItemManager struct{
-	items map[string]*Item
+type ItemManager struct {
+	items    map[string]*Item
 	basePath string
 }
 
-func (i *ItemManager) checkExists(path string){
-	_,err := os.Stat(path)
-	if os.IsNotExist(err){
+func (i *ItemManager) checkExists(path string) {
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
 		i.initExample()
 	}
 }
 
-func (i *ItemManager) initExample(){
-	if err := os.MkdirAll(i.basePath,os.ModePerm);err != nil{
+func (i *ItemManager) initExample() {
+	if err := os.MkdirAll(i.basePath, os.ModePerm); err != nil {
 		panic(err)
 	}
-	var defaultItems []*Item =[]*Item{
+	var defaultItems []*Item = []*Item{
 		&Item{
-			Host:"*.z3q.net",
-			AllLocation:"http://www.z3q.net",
+			Host:        "*.z3q.net",
+			AllLocation: "http://www.z3q.net",
 		},
 		&Item{
-			Host:"s.z3q.net",
-			Map:map[string]string{
-				"blog":"http://www.s1n1.com",
+			Host: "s.z3q.net",
+			Map: map[string]string{
+				"blog": "http://www.s1n1.com",
 			},
 		},
 	}
-	bytes,_ := json.MarshalIndent(defaultItems," "," ")
+	bytes, _ := json.MarshalIndent(defaultItems, " ", " ")
 
-	f,_ := os.Create(i.basePath+"gourl.conf");
+	f, _ := os.Create(i.basePath + "gourl.conf")
 	defer f.Close()
 	wr := bufio.NewWriter(f)
 	wr.Write(bytes)
 	wr.Flush()
 }
 
-func (this *ItemManager) Load(){
+func (this *ItemManager) Load() {
 	this.checkExists(this.basePath)
-	filepath.Walk(this.basePath,func(path string, info os.FileInfo, err error)error{
-		if err == nil && !info.IsDir() && strings.HasSuffix(path,".conf"){
+	filepath.Walk(this.basePath, func(path string, info os.FileInfo, err error) error {
+		if err == nil && !info.IsDir() && strings.HasSuffix(path, ".conf") {
 			items := this.GetItemsFromFile(path)
 			this.Append(items)
 		}
@@ -96,59 +97,60 @@ func (this *ItemManager) Load(){
 	})
 }
 
-func (this *ItemManager) GetItemsFromFile(path string)[]*Item {
+func (this *ItemManager) GetItemsFromFile(path string) []*Item {
 	bytes, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil
 	}
-	var items []*Item=make([]*Item,0)
-	err = json.Unmarshal(bytes,&items)
-	if err != nil{
-		panic(path+" - "+err.Error())
+	var items []*Item = make([]*Item, 0)
+	err = json.Unmarshal(bytes, &items)
+	if err != nil {
+		panic(path + " - " + err.Error())
 	}
 	return items
 }
 
-func (this *ItemManager) Append(items []*Item){
-	if this.items == nil{
-		this.items = make(map[string]*Item,0)
+func (this *ItemManager) Append(items []*Item) {
+	if this.items == nil {
+		this.items = make(map[string]*Item, 0)
 	}
-	if items!= nil {
-		for _,v := range items {
+	if items != nil {
+		for _, v := range items {
 			if _, ok := this.items[v.Host]; ok {
-				panic("has exists host "+v.Host)
+				panic("has exists host " + v.Host)
 			}
 			this.items[v.Host] = v
 		}
 	}
 }
 
-func (this *ItemManager) GetItemByHost(host string)*Item{
-	for k,v := range this.items {
-		if this.matchHost(k,host){
+func (this *ItemManager) GetItemByHost(host string) *Item {
+	for k, v := range this.items {
+		if this.matchHost(k, host) {
 			return v
 		}
 	}
 	return nil
 }
 
-func (this *ItemManager) matchHost(key,host string)bool{
-	if host == key{
+func (this *ItemManager) matchHost(key, host string) bool {
+	if host == key {
 		return true
 	}
-	if strings.HasPrefix(key, "*."){
-		return strings.HasSuffix(host,key[2:])
+	if strings.HasPrefix(key, "*.") {
+		return strings.HasSuffix(host, key[2:])
 	}
 	return false
 }
 
 var _ http.Handler = new(redirectHandler)
-type redirectHandler struct{
+
+type redirectHandler struct {
 	itemManager *ItemManager
 }
 
-func (r *redirectHandler) ServeHTTP(rsp http.ResponseWriter,req *http.Request) {
-	host := req.Host; // "s.z3q.net" use for test
+func (r *redirectHandler) ServeHTTP(rsp http.ResponseWriter, req *http.Request) {
+	host := req.Host // "s.z3q.net" use for test
 	var item *Item = r.itemManager.GetItemByHost(host)
 	if item != nil {
 		if location, b := r.getLocation(rsp, req, item); b {
@@ -160,7 +162,7 @@ func (r *redirectHandler) ServeHTTP(rsp http.ResponseWriter,req *http.Request) {
 	rsp.Write([]byte("Not match any host"))
 }
 
-func (r *redirectHandler) getLocation(rsp http.ResponseWriter,req *http.Request,item *Item)(string,bool) {
+func (r *redirectHandler) getLocation(rsp http.ResponseWriter, req *http.Request, item *Item) (string, bool) {
 	path := req.URL.Path
 	query := req.URL.RawQuery
 	var con string
@@ -170,17 +172,16 @@ func (r *redirectHandler) getLocation(rsp http.ResponseWriter,req *http.Request,
 	if len(item.AllLocation) != 0 {
 		return fmt.Sprintf("%s%s%s%s", item.AllLocation, path, con, query), true
 	}
-	if v,ok := item.Map[path[1:]]; ok {
+	if v, ok := item.Map[path[1:]]; ok {
 		return fmt.Sprintf("%s%s%s", v, con, query), true
 	}
 	return "", false
 }
 
-
-func getHandler()http.Handler {
+func getHandler() http.Handler {
 	re := &redirectHandler{
 		itemManager: &ItemManager{
-			basePath:ConfBasePath,
+			basePath: ConfBasePath,
 		},
 	}
 	re.itemManager.Load()
